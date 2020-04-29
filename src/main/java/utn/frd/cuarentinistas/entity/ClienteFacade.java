6c834437a404efaa49e5688a32f0f66455358e77
lucio <lucio@192.168.0.100>
2020-04-29 19:28:14 -0300

    initial commit

---
+++ src/main/java/utn/frd/cuarentinistas/entity/ClienteFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utn.frd.cuarentinistas.entity;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author lucio
+ */
+@Stateless
+public class ClienteFacade extends AbstractFacade<Cliente> {
+
+    @PersistenceContext(unitName = "my_persistence_unit")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ClienteFacade() {
+        super(Cliente.class);
+    }
+    
+}
