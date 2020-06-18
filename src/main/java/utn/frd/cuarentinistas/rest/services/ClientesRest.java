028547e3a0d87b7941221ac7906b469403eb0a2e
FrancoARossi <rossifrancoa@gmail.com>
2020-06-18 11:44:01 -0300

    Agregado ClientesRest

---
+++ src/main/java/utn/frd/cuarentinistas/rest/services/ClientesRest.java
@@ -0,0 +1,60 @@
+package utn.frd.cuarentinistas.rest.services;
+
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import utn.frd.cuarentinistas.entities.Clientes;
+import utn.frd.cuarentinistas.sessions.ClientesFacade;
+
+
+@Path("/clientes")
+public class ClientesRest {
+    @EJB
+    private ClientesFacade ejbClientesFacade;
+
+    //obtener todas las entidades
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Clientes> findAll(){
+        return ejbClientesFacade.findAll();
+    }
+
+    //crear entidades
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(Clientes cliente){
+        ejbClientesFacade.create(cliente);
+    }
+
+    //actualizar entidades
+    @PUT
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Path("/{id}")
+    public void edit(@PathParam("id")long id, Clientes cliente){
+        ejbClientesFacade.edit(cliente);
+    }
+
+    //eliminar entidades
+    @DELETE
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    @Path("/{id}")
+    public void remove(@PathParam("id")long id){
+        ejbClientesFacade.remove( ejbClienteFacade.find(id) );
+    }
+
+    //obtener una entidad por id
+    @GET
+    @Path("/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Clientes findById(@PathParam("id")long id){
+        return ejbClientesFacade.find(id);
+    }
+}
