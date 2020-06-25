2f0b213406f9466d15b8296bb72d49535d1af99e
lucioserra <lucioserra15@gmail.com>
2020-06-25 16:31:43 -0300

    prueba obtener clientes

--- src/main/webapp/js/obtenerClientes.js
+++ src/main/webapp/js/obtenerClientes.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.onload=function(){
+    var clientes = document.getElementById("clientes");
+    clientes.addEventListener("click", traerDatos);
+};
 
-
+function traerDatos(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',"http://localhost:8080/cuarentinistas/rest/clientes",true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+        };
+    };
+}
