961e664fdcfab2f6783df95a9159c6dbeb8763f3
FrancoARossi <rossifrancoa@gmail.com>
2020-06-25 20:39:07 -0300

    obtenerClientes funcionando como una prueba

--- src/main/webapp/js/obtenerClientes.js
+++ src/main/webapp/js/obtenerClientes.js
@@ -9,7 +9,7 @@ function buscarDatos(){
     xhr.send();
     xhr.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200){
-            var resultado = this.responseText;
+            var resultado = JSON.parse(this.responseText);
             mostrarDatos(resultado);
         };
     };
@@ -17,6 +17,15 @@ function buscarDatos(){
 
 function mostrarDatos(resultado) {
     var pantalla = document.getElementById("pantalla");
-    pantalla.innerHTML = "<ul>" + resultado + "</p>";
+    var i, j;
+    var datos = "";
+    for (i = 0; i < resultado.length; i++){
+        datos += "<li>" + resultado[i].nombre + "</li>";
+        datos += "<li>" + resultado[i].apellido + "</li>";
+        datos += "<li>" + resultado[i].documento + "</li>";
+        datos += "<li>" + resultado[i].fecha_nac + "</li>";
+        datos += "<li>" + resultado[i].direccion + "</li>";
+    }
+    pantalla.innerHTML = "<ul>" + datos + "</ul>";
     console.log("It Works!");
 }
